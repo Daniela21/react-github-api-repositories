8c6b26f57ded9c786f3913f895676f304fbebf79
Daniela Mondragon <danamrosas2@gmail.com>
2019-03-13 03:44:46 -0600

    Inicialice the app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Title } from './components/Title'
+import { SearchForm } from './components/SearchForm'
+import { Repositories } from './components/Repositories'
+
 import './App.css';
+import 'bulma/css/bulma.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {
+        usedSearch: false,
+        results: []
+    }
+
+    _handleResults = (results) =>{
+        this.setState({ results, usedSearch: true })
+    }
+
+    _renderResults () {
+        return this.state.results.length === 0 
+            ? <p>Sorry!<span role="img" aria-label="Snowman">😞</span>Results not found</p>
+            : <Repositories results={this.state.results}/>
+        
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Title>Search Repositories</Title>
+                <div className="SearchForm-wrapper">
+                    <SearchForm onResults={this._handleResults} />
+                </div>
+                {
+                    this.state.usedSearch
+                        ? this._renderResults()
+                        : <small>Type a username in the form</small>
+                }
+            </div>
+        )
+    }
 }
 
 export default App;
